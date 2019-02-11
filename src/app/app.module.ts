15cae4884ab27c49948c04e68065e638e6b4ec3e
AizenIlang <aizen@live.com.ph>
2019-02-12 00:45:46 +0800

    added routing in the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,15 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forChild([
+      {
+        path : '',
+        component : AppComponent
+      }
+         
+      
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
