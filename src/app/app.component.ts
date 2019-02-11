1cc27fd9dc5f0347b6e0f1b0d6cf77fe0d24fb6d
AizenIlang <aizen@live.com.ph>
2019-02-12 00:24:53 +0800

    Added Forms Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practicewithjim';
+  title = 'PRACTICE WITH JIM';
+
+  Username : any;
+  Password : any;
+
+
+  Login(){
+    this.title = "Your Username is "+ this.Username + " Password is " + this.Password;
+  }
 }
